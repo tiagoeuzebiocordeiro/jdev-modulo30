d9e526a7f5ae52d22881593f5ca83bc5e7bc0cc5
Tiago Cordeiro <tiagoezc@hotmail.com>
2022-11-21 20:59:21 -0300

    first commit

---
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -0,0 +1,18 @@
+package projeto.spring.data.aula;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
+public class AppSpringDataTest {
+
+	@Test
+	public void testeInsert() {
+
+
+	}
+
+}
